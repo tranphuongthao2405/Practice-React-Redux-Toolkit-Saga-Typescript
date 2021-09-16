69ffb3ee22f7dd0f887e77293781fcfc9d6ebbb2
tranphuongthao2405 <tranphuongthao24051999@gmail.com>
2021-09-16 18:41:32 +0700

    first project

---
+++ react-redux-ts1/src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from 'redux';
+import { actionCreators, State } from './state';
+
+function App() {
+  const dispatch = useDispatch();
+  const amount = useSelector((state: State) => state.bank);
+
+  const { depositMoney, withdrawMoney, bankrupt } = bindActionCreators(actionCreators, dispatch);
+
+  return (
+    <div className="App">
+      <h1>{amount}</h1>
+      <button onClick={() => depositMoney(1000)}>Deposit</button>
+      <button onClick={() => withdrawMoney(500)}>Withdraw</button>
+      <button onClick={() => bankrupt()}>Bankrupt</button>
+    </div>
+  );
+}
+
+export default App;
