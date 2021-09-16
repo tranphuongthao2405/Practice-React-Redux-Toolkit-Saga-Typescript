a14f2076b81cc3f676c8a8a43832a60e4361d045
tranphuongthao2405 <tranphuongthao24051999@gmail.com>
2021-09-17 05:22:24 +0700

    lab 2

--- ReactHooks/usestate-lab1/src/App.js
+++ ReactHooks/usestate-lab1/src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
-import ColorBox from './components/ColorBox';
+import { useState } from "react";
+import "./App.css";
+import ColorBox from "./components/ColorBox/ColorBox";
+import TodoList from "./components/TodoList/TodoList";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "I love Easy Frontend! 😍 " },
+    { id: 2, title: "We love Easy Frontend! 🥰 " },
+    { id: 3, title: "They love Easy Frontend! 🚀 " },
+  ]);
+
+  const handleTodoClick = (todo) => {
+    const index = todoList.findIndex((x) => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  };
+
   return (
     <div className="App">
-      <ColorBox />
+      {/* <ColorBox /> */}
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
